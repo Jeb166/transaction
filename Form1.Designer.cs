d5c6959deb40d711b1fc2e145692f20c4fa1b82c
Emre <emrebas02@hotmail.com>
2024-05-20 00:43:04 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace transaction
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components!= null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStartSimulation = new System.Windows.Forms.Button();
+            this.comboBoxIsolationLevel = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudTypeAUsers = new System.Windows.Forms.NumericUpDown();
+            this.nudTypeBUsers = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTypeAUsers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTypeBUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStartSimulation
+            // 
+            this.buttonStartSimulation.Location = new System.Drawing.Point(18, 160);
+            this.buttonStartSimulation.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonStartSimulation.Name = "buttonStartSimulation";
+            this.buttonStartSimulation.Size = new System.Drawing.Size(180, 35);
+            this.buttonStartSimulation.TabIndex = 0;
+            this.buttonStartSimulation.Text = "Start Simulation";
+            this.buttonStartSimulation.UseVisualStyleBackColor = true;
+            this.buttonStartSimulation.Click += new System.EventHandler(this.buttonStartSimulation_Click);
+            // 
+            // comboBoxIsolationLevel
+            // 
+            this.comboBoxIsolationLevel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxIsolationLevel.FormattingEnabled = true;
+            this.comboBoxIsolationLevel.Items.AddRange(new object[] {
+            "ReadUncommitted",
+            "ReadCommitted",
+            "RepeatableRead",
+            "Serializable",
+            "Snapshot"});
+            this.comboBoxIsolationLevel.Location = new System.Drawing.Point(18, 118);
+            this.comboBoxIsolationLevel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBoxIsolationLevel.Name = "comboBoxIsolationLevel";
+            this.comboBoxIsolationLevel.Size = new System.Drawing.Size(178, 28);
+            this.comboBoxIsolationLevel.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(207, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Type A Users:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(207, 89);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Type B Users:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(207, 123);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Isolation Level:";
+            // 
+            // nudTypeAUsers
+            // 
+            this.nudTypeAUsers.Location = new System.Drawing.Point(18, 43);
+            this.nudTypeAUsers.Name = "nudTypeAUsers";
+            this.nudTypeAUsers.Size = new System.Drawing.Size(178, 26);
+            this.nudTypeAUsers.TabIndex = 7;
+            // 
+            // nudTypeBUsers
+            // 
+            this.nudTypeBUsers.Location = new System.Drawing.Point(18, 82);
+            this.nudTypeBUsers.Name = "nudTypeBUsers";
+            this.nudTypeBUsers.Size = new System.Drawing.Size(178, 26);
+            this.nudTypeBUsers.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 592);
+            this.Controls.Add(this.nudTypeBUsers);
+            this.Controls.Add(this.nudTypeAUsers);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxIsolationLevel);
+            this.Controls.Add(this.buttonStartSimulation);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Transaction Simulation";
+            ((System.ComponentModel.ISupportInitialize)(this.nudTypeAUsers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudTypeBUsers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStartSimulation;
+        private System.Windows.Forms.ComboBox comboBoxIsolationLevel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudTypeAUsers;
+        private System.Windows.Forms.NumericUpDown nudTypeBUsers;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Threading;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace transaction
+{
+    public partial class Form1 : Form
+    {
+        private string _connectionString = "Server=G513;Database=AdventureWorks2019;Trusted_Connection=True;TrustServerCertificate=true;";
+        private IsolationLevel _isolationLevel;
+        private int a_deadlockCount = 0;
+        private int b_deadlockCount = 0;
+        private double _totalDurationTypeA = 0;
+        private double _totalDurationTypeB = 0;
+        private int _typeACount = 0;
+        private int _typeBCount = 0;
+        private object _lockObject = new object();
+        Random rand = new Random();  // Create a single Random object here
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStartSimulation_Click(object sender, EventArgs e)
+        {
+            _isolationLevel = (IsolationLevel)Enum.Parse(typeof(IsolationLevel), comboBoxIsolationLevel.SelectedItem.ToString());
+
+            int typeAUsers = Convert.ToInt32(nudTypeAUsers.Value);
+            int typeBUsers = Convert.ToInt32(nudTypeBUsers.Value);
+
+            List<Thread> threads = new List<Thread>();
+
+            for (int i = 0; i < typeAUsers; i++)
+            {
+                Thread thread = new Thread(() => TypeAUserThread());
+                threads.Add(thread);
+                thread.Start();
+            }
+
+            for (int i = 0; i < typeBUsers; i++)
+            {
+                Thread thread = new Thread(() => TypeBUserThread());
+                threads.Add(thread);
+                thread.Start();
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+
+            CalculateAndPrintAverageDurations();
+            Console.WriteLine("Deadlocks occured by Type A Threads: {0}", a_deadlockCount);
+            Console.WriteLine("Deadlocks occured by Type A Threads: {0}", b_deadlockCount);
+        }
+
+        private void TypeAUserThread()
+        {
+            DateTime startTime = DateTime.Now;
+
+            for (int i = 0; i < 100; i++)
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+
+                    SqlTransaction transaction = connection.BeginTransaction(_isolationLevel);
+
+                    try
+                    {
+                        if (rand.NextDouble() < 0.5)  // Use the same Random object
+                        {
+                            RunUpdateQuery(connection, transaction, "20110101", "20111231");
+                        }
+                        if (rand.NextDouble() < 0.5)
+                        {
+                            RunUpdateQuery(connection, transaction, "20120101", "20121231");
+                        }
+                        if (rand.NextDouble() < 0.5)
+                        {
+                            RunUpdateQuery(connection, transaction, "20130101", "20131231");
+                        }
+                        if (rand.NextDouble() < 0.5)
+                        {
+                            RunUpdateQuery(connection, transaction, "20140101", "20141231");
+                        }
+                        if (rand.NextDouble() < 0.5)
+                        {
+                            RunUpdateQuery(connection, transaction, "20150101", "20151231");
+                        }
+
+                        transaction.Commit();
+
+                    }
+                    catch (SqlException ex)
+                    {
+                        transaction.Rollback();
+                        Interlocked.Increment(ref a_deadlockCount);
+                        // Handle error
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+
+            TimeSpan elapsedTime = DateTime.Now - startTime;
+            lock (_lockObject)
+            {
+                _totalDurationTypeA += elapsedTime.TotalMilliseconds;
+                _typeACount++;
+            }
+        }
+
+
+        private void TypeBUserThread()
+        {
+            DateTime startTime = DateTime.Now;
+
+            for (int i = 0; i < 100; i++)
+            {
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+
+                    SqlTransaction transaction = connection.BeginTransaction(_isolationLevel);
+
+                    try
+                    {
+                        if (rand.NextDouble() < 0.5)
+                        {
+                            RunSelectQuery(connection, transaction, "20110101", "20111231");
+                        }
+                        if (rand.NextDouble() < 0.5)
+                        {
+                            RunSelectQuery(connection, transaction, "20120101", "20121231");
+                        }
+                        if (rand.NextDouble() < 0.5)
+                        {
+                            RunSelectQuery(connection, transaction, "20130101", "20131231");
+                        }
+                        if (rand.NextDouble() < 0.5)
+                        {
+                            RunSelectQuery(connection, transaction, "20140101", "20141231");
+                        }
+                        if (rand.NextDouble() < 0.5)
+                        {
+                            RunSelectQuery(connection, transaction, "20150101", "20151231");
+                        }
+
+                        transaction.Commit();
+
+
+                    }
+                    catch (SqlException ex)
+                    {
+                        transaction.Rollback();
+                        Interlocked.Increment(ref b_deadlockCount);
+                        // Handle error
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+
+
+            TimeSpan elapsedTime = DateTime.Now - startTime;
+            lock (_lockObject)
+            {
+                _totalDurationTypeB += elapsedTime.TotalMilliseconds;
+                _typeBCount++;
+            }
+        }
+
+        private void CalculateAndPrintAverageDurations()
+        {
+            double averageDurationTypeA = _totalDurationTypeA / _typeACount;
+            double averageDurationTypeB = _totalDurationTypeB / _typeBCount;
+
+            Console.WriteLine("Average Duration of Type A Threads: {0} ms", averageDurationTypeA);
+            Console.WriteLine("Average Duration of Type B Threads: {0} ms", averageDurationTypeB);
+        }
+
+        private void RunUpdateQuery(SqlConnection connection, SqlTransaction transaction, string beginDate, string endDate)
+        {
+            using (SqlCommand command = new SqlCommand("UPDATE Sales.SalesOrderDetail SET UnitPrice = UnitPrice * 10.0 / 10.0 WHERE UnitPrice > 100 AND EXISTS (SELECT * FROM Sales.SalesOrderHeader WHERE Sales.SalesOrderHeader.SalesOrderID = Sales.SalesOrderDetail.SalesOrderID AND Sales.SalesOrderHeader.OrderDate BETWEEN @BeginDate AND @EndDate AND Sales.SalesOrderHeader.OnlineOrderFlag = 1)", connection, transaction))
+            {
+                command.Parameters.AddWithValue("@BeginDate", beginDate);
+                command.Parameters.AddWithValue("@EndDate", endDate);
+
+                try
+                {
+                    command.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    if (ex.Number == 1205) // Deadlock
+                    {
+                        transaction.Rollback();
+                        Interlocked.Increment(ref a_deadlockCount); // Increment global deadlock count
+                        RunUpdateQuery(connection, transaction, beginDate, endDate);
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+
+        private void RunSelectQuery(SqlConnection connection, SqlTransaction transaction, string beginDate, string endDate)
+        {
+            using (SqlCommand command = new SqlCommand("SELECT SUM(Sales.SalesOrderDetail.OrderQty) FROM Sales.SalesOrderDetail WHERE UnitPrice > 100 AND EXISTS (SELECT * FROM Sales.SalesOrderHeader WHERE Sales.SalesOrderHeader.SalesOrderID = Sales.SalesOrderDetail.SalesOrderID AND Sales.SalesOrderHeader.OrderDate BETWEEN @BeginDate AND @EndDate AND Sales.SalesOrderHeader.OnlineOrderFlag = 1)", connection, transaction))
+            {
+                command.Parameters.AddWithValue("@BeginDate", beginDate);
+                command.Parameters.AddWithValue("@EndDate", endDate);
+
+                try
+                {
+                    command.ExecuteScalar();
+                }
+                catch (SqlException ex)
+                {
+                    if (ex.Number == 1205) // Deadlock
+                    {
+                        transaction.Rollback();
+                        Interlocked.Increment(ref b_deadlockCount); // Increment global deadlock count
+                        RunSelectQuery(connection, transaction, beginDate, endDate);
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
